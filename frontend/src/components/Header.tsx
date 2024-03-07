0ae09790cefaeb764a0bccab391e6feb5c0a68a5
Mike Rushton <mrush94ton@gmail.com>
2024-03-07 15:49:41 +0000

    created header and made it responsive

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 
 export default function Header() {
@@ -8,6 +10,12 @@ export default function Header() {
             <Link to='/' className="text-3xl font-bold tracking-tight text-orange-500">
                 RushEats.com
             </Link>
+            <div className="md:hidden">
+                <MobileNav />
+            </div>
+            <div className="hidden md:block">
+                <MainNav />
+            </div>
         </div>
     </div>
   )
